0d48216f7b62a6890c4767d70004c22fee2a4756
Erhan <84393455+Erhanero@users.noreply.github.com>
2021-06-05 20:43:39 +0300

    Create commonElements.js

---
+++ JS Fundamentals/Common elements/commonElements.js
@@ -0,0 +1,14 @@
+function commonElements(array1, array2) {
+
+
+    for (let i = 0; i < array1.length; i++) {
+        let firstElement = array1[i];
+        for (let j = 0; j < array2.length; j++) {
+            let secondElement = array2[j]
+            if (firstElement === secondElement) {
+                console.log(firstElement)
+            }
+        }
+
+    }
+}
