ad167acc08533f22ce6e3daf3ed57e559b742254
Erhan <84393455+Erhanero@users.noreply.github.com>
2021-10-12 18:27:53 +0200

    Create tickets.js

---
+++ JS Advanced/DOM Manipulations and Events/tickets.js
@@ -0,0 +1,32 @@
+function solve(arr, sortingCriterion) {
+    let obj = {};
+    let tickets = [];
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+    }
+
+    for (let el of arr) {
+        let [destination, price, status] = el.split("|");
+        price = Number(price);
+
+        let ticket = new Ticket(destination, price, status);
+        tickets.push(ticket)
+
+    }
+
+    if (sortingCriterion == "price") {
+        tickets = tickets.sort((a, b) => a.price - b.price);
+    } else if (sortingCriterion == "destination") {
+
+        tickets = tickets.sort((a, b) => a.destination.localeCompare(b.destination))
+    } else if (sortingCriterion == "status") {
+        tickets = tickets.sort((a, b) => a.status.localeCompare(b.status))
+
+    }
+    return tickets;
+
+}
