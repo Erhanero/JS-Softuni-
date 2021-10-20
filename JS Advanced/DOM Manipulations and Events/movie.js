163a6747e74549866a24a96de8bf88fa08782e44
Erhan <84393455+Erhanero@users.noreply.github.com>
2021-10-20 18:46:12 +0200

    Update movie.js

--- JS Advanced/DOM Manipulations and Events/movie.js
+++ JS Advanced/DOM Manipulations and Events/movie.js
@@ -22,6 +22,7 @@ class Movie {
         }
 
     }
+    
     endScreening(date, hall, soldTickets) {
         let screen = this._screenings.find((x => x.date == date && x.hall == hall));
 
@@ -64,18 +65,3 @@ class Movie {
 
     }
 }
-
-let m = new Movie('Wonder Woman 1984', '10.00');
-console.log(m.newScreening('October 2, 2020', 'IMAX 3D', `3D`));
-console.log(m.newScreening('October 3, 2020', 'Main', `regular`));
-console.log(m.newScreening('October 4, 2020', 'IMAX 3D', `3D`));
-console.log(m.endScreening('October 2, 2020', 'IMAX 3D', 150));
-console.log(m.endScreening('October 3, 2020', 'Main', 78));
-console.log(m.toString());
-
-m.newScreening('October 4, 2020', '235', `regular`);
-m.newScreening('October 5, 2020', 'Main', `regular`);
-m.newScreening('October 3, 2020', '235', `regular`);
-m.newScreening('October 4, 2020', 'Main', `regular`);
-console.log(m.toString());
-
